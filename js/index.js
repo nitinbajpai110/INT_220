b4621189a78ff208043de1e351808c9c156c14cf
Nitin Bajpai <nitinbajpai110@gmail.com>
2020-03-29 20:19:02 +0530

    js files

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+document.getElementById('allpost').addEventListener("click",function(){
+    location.href="html/bloglist.html";
+})
+document.getElementById('createpost').addEventListener("click",function(){
+    location.href="html/creat.html";
+})
