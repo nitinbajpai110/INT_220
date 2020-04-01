292a1b25bd3c3c607d4ece002e546bc593635e89
Nitin Bajpai <nitinbajpai110@gmail.com>
2020-04-01 17:14:34 +0530

    login signup features with database connectivity

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href='css\header.css' rel='stylesheet' type='text/css'>
+    <link href='css\index.css' rel='stylesheet' type='text/css'>
+    <link href='css\font.css' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css\bootstrap.min.css">
+
+</head>
+<body link="blue">
+    <div id="main-container-div">
+
+        <div class="header">
+
+            <a href="register.php" class="topButtons1" type="button">Sign Up</a>
+            <a href="login.php" class="topButtons2" type="button"> Sign in</a>
+            <a id="pageTitle" class="logo-title">WeEBlY</a>
+            <p class="middleText">Explore Imagine Create</p>
+          
+        </div>
+        <div class="postButtons">
+          
+          <button type="button" id="allpost">All Posts</button>
+          <button type="button" id="createpost">Create Post</button>
+          
+        </div>
+    </div>
+    
+    <script src="js/jquery.js"></script>
+ 
+    <script src="js/bootstrap.min.js"></script>
+	<!-- Place all Javascript for header in this file -->
+	<script src="js\header.js"></script>
+	<!-- Place all Javascript for rest of the elements in this file -->
+    <script src="js\index.js"></script>
+    
+</body>
+</html>
+
+
+
